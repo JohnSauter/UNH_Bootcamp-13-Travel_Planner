c82cb9579e189f8804bb2ec06b6a4211e0b7872a
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-15 14:40:34 -0400

    traveller routes work

--- routes/api/traveller_routes.js
+++ routes/api/traveller_routes.js
@@ -41,4 +41,23 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// DELETE a traveller
+router.delete('/:id', async (req, res) => {
+  try {
+    const traveller_data = await Traveller.findByPk(req.params.id);
+    if (!traveller_data) {
+      res.status(404).json({ message: 'No traveller found with that id!' });
+      return;
+    }
+    const result = await Traveller.destroy({
+      where: {
+        id: traveller_data.id
+      }
+    })
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
