0375f795123f86b3b9508e33b733fa178fc1b1b3
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-06-15 12:52:50 -0400

    include work done during class

---
+++ models/Trip.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'trip',
+  }
+);
+
+module.exports = Trip;
